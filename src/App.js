fe55885a4f217685755e38a89cfaecbe7164505e
Chase00 <chasekuhnmc@gmail.com>
2020-07-10 18:01:32 -0400

    Searching for recipes
    Submiting text in the text box now queries the API for recipes

--- src/App.js
+++ src/App.js
@@ -8,24 +8,42 @@ const App = () => {
   const APP_KEY = process.env.REACT_APP_KEY;
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
    getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await
-      fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
 
       const data = await response.json();
       setRecipes(data.hits);
   }
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+
   return (
     <div>
       <h1>Recipe Booklet</h1>
-        <form className="search-form">
-          <input className="search-bar" type="text"/>
+        <form onSubmit={getSearch} className="search-form">
+          <input 
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch} 
+            />
           <button className="search-button" type="submit">Submit</button>
         </form>
         {recipes.map(r => (
