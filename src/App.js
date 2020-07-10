699d55f94688d455eb647dd9af32eb1a2c340788
Chase00 <chasekuhnmc@gmail.com>
2020-07-10 16:56:07 -0400

    env api keys
    API: https://developer.edamam.com/

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = process.env.REACT_APP_ID
+  const APP_KEY = process.env.REACT_APP_KEY;
+
   return (
     <div>
       <h1>Recipe Booklet</h1>
