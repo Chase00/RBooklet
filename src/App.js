26969f23cb99de19f84ac06d120c39ce1d80a7c6
Chase00 <chasekuhnmc@gmail.com>
2020-07-11 01:29:22 -0400

    Material-ui styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import { Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 import Recipe from "./Recipe";
+import 'fontsource-roboto';
 import './App.css';
 
 const App = () => {
@@ -21,6 +27,8 @@ const App = () => {
 
       const data = await response.json();
       setRecipes(data.hits);
+      console.log(data.hits)
+      setSearch("")
   }
 
   const updateSearch = (e) => {
@@ -33,27 +41,50 @@ const App = () => {
     setQuery(search);
   }
 
+  const style = {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: 'white'
+  };
+
 
   return (
     <div>
-      <h1>Recipe Booklet</h1>
+      <Card>
+      <CardContent>
+      <Typography variant="h2" className="header" gutterBottom>Recipe Booklet</Typography>
+      <Typography variant="subtitle1" gutterBottom>Enter a food or meal type to get results for different recipes</Typography>
+  
         <form onSubmit={getSearch} className="search-form">
-          <input 
-            className="search-bar"
+
+          <TextField 
+            id="standard-required"
+            label="Food"
             type="text"
             value={search}
-            onChange={updateSearch} 
-            />
-          <button className="search-button" type="submit">Submit</button>
+            onChange={updateSearch}
+            required 
+          />
+          <br />
+          <br />
+
+          <Button style={style} variant="contained" type="submit">Get Recipes</Button>
         </form>
+        </CardContent>
+        </Card>
+
+        <br />
+        
         {recipes.map(r => (
           <Recipe 
             title={r.recipe.label}
             calories={r.recipe.calories} 
             image={r.recipe.image}
             key={`${r.recipe.label} recipe`}
+            ingredients={r.recipe.ingredients}
             />
         ))}
+        <br />
+        <br />
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const Recipe = ({title, calories, image, key}) => {
-    return(
+
+const Recipe = ({ title, calories, image, key, ingredients }) => {
+    return (
         <div>
-            <h1>{title}</h1>
-            <p>{calories}</p>
-            <img src={image} alt={key} />
+        <Card>
+            <CardContent>
+                <Typography variant="h5" gutterBottom>{title}</Typography>
+                <Typography variant="subtitle1" gutterBottom>({calories} Calories)</Typography>
+                <img src={image} alt={key} />
+
+                <Accordion className="accordion">
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography variant="subtitle1">Ingredients</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            <ul>
+                                {ingredients.map(ingredient => (
+                                    <Typography variant="subtitle1" gutterBottom>{ingredient.text}</Typography>
+                                ))}
+                            </ul>
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            </CardContent>
+        </Card>
+        <br />
         </div>
     );
 }
